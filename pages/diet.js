a013b4a7a25fc0c22c5e6712f6713292b50e7359
Will McCracken <wem5637@gmail.com>
2021-07-29 11:07:04 -0500

    header to own comp

--- pages/diet.js
+++ pages/diet.js
@@ -4,21 +4,11 @@ import styles from '../styles/Home.module.css'
 import profilePic from '../public/pug-icon-0.png'
 import pugPic from './../public/pug-doughnut.jpg';
 import Link from 'next/link';
+import Header from './components/Header';
 
 export default ()=>{
   return <div>
-      <div class="header" style={{ width: "100%", display:"flex", border: "dashed 5px bisque", backgroundColor:"salmon", overflow: "hidden", padding: "10px"}}>
-        <a href="#default" class="logo" style={{  float: "left", color: "black", textAlign: "center", textDecoration: "none", fontSize: "18px", lineHeight: "25px", borderRadius: "4px"}}>
-        <Image height="100" width="100" src={profilePic} alt="Picture of the author" />
-        </a>
-        <div class="header-right" style={{margin:"10px", color:"bisque"}}>
-          <Link href="/"><a style={{ margin: '10px', color:"bisque"}}>Home</a></Link>
-          <a href="#contact" style={{margin:"10px", color:"bisque"}}>Contact</a>
-          <Link href="/image-gallery"><a style={{ margin: '10px', color:"bisque"}}>Image Gallery</a></Link>
-          <Link href="/diet"><a style={{ margin: '10px', color:"bisque"}}>Diet</a></Link>
-        </div>
-      </div>
-
+      <Header/>
       <main>
         <h1>Diet</h1>
       </main>
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import profilePic from '../public/pug-icon-0.png'
 import pugPic from './../public/pug-doughnut.jpg';
 import Link from 'next/link';
+import Header from './components/Header';
 
 export default function Home() {
   return (
@@ -14,19 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div class="header" style={{ width: "100%", display:"flex", border: "dashed 5px bisque", backgroundColor:"salmon", overflow: "hidden", padding: "10px"}}>
-        <a href="#default" class="logo" style={{  float: "left", color: "black", textAlign: "center", textDecoration: "none", fontSize: "18px", lineHeight: "25px", borderRadius: "4px"}}>
-        <Image height="100" width="100" src={profilePic} alt="Picture of the author" />
-        </a>
-        <div class="header-right" style={{margin:"10px", color:"bisque"}}>
-          <Link href="/"><a style={{ margin: '10px', color:"bisque"}}>Home</a></Link>
-          <a href="#contact" style={{margin:"10px", color:"bisque"}}>Contact</a>
-          <Link href="/image-gallery"><a style={{ margin: '10px', color:"bisque"}}>Image Gallery</a></Link>
-          <Link href="/diet"><a style={{ margin: '10px', color:"bisque"}}>Diet</a></Link>
-        </div>
-      </div>
-
-
+      <Header/>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <strong>PugFlex</strong>!
