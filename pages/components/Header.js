67cf3f061776a074fe03300a5a33d39a69ee85a1
Will McCracken <wem5637@gmail.com>
2021-07-30 12:53:10 -0500

    spin animation

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -2,14 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import profilePic from '../../public/pug-icon-0.png'
 import Link from 'next/link';
+import '../../styles/Home.module.css'
 
 // eslint-disable-next-line react/display-name
 export default function Header() {
   return (
   <div className="header" style={{ width: "100%", display:"flex", border: "dashed 5px bisque", backgroundColor:"salmon", overflow: "hidden", padding: "10px"}}>
-    <a href="/" className="logo" style={{  float: "left", color: "black", textAlign: "center", textDecoration: "none", fontSize: "18px", lineHeight: "25px", borderRadius: "4px"}}>
-      <Image height="100" width="100" src={profilePic} alt="Picture of the author" />
-    </a>
+    <div className="logo">
+      <a href="/" style={{  float: "left", color: "black", textAlign: "center", textDecoration: "none", fontSize: "18px", lineHeight: "25px", borderRadius: "4px"}}>
+        <Image height="100" width="100" src={profilePic} alt="Picture of the author" />
+      </a>
+    </div>
     <div className="header-right" style={{
       display: "flex",
       flexWrap: "wrap",
